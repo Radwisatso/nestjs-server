db64687b549adec3f3319a2d9683f32b08be37e5
Raditya Dwisatrio Santoso <dwisatrioraditya@gmail.com>
2023-07-06 17:03:56 +0700

    Adding CORS

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { TransformInterceptor } from './transform.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors()
   app.useGlobalPipes(new ValidationPipe())
   app.useGlobalInterceptors(new TransformInterceptor()) //Not working yet
   await app.listen(process.env.PORT || 3000);
